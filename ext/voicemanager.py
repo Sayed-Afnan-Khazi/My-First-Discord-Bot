d10d8feb2f85e19ea674c35c72aaa4b0bc6a4bb0
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2022-12-20 16:08:19 +0530

    Fixed invoice,  started on multiple channels

--- ext/voicemanager.py
+++ ext/voicemanager.py
@@ -2,10 +2,10 @@ import os
 from discord.ext import commands
 import discord
 
-
 AUDIENCE_ROLE_ID = 1045963196881707009 
 PERFORMER_ROLE_ID = 1045963339634835507
 GENERAL_CHANNEL_ID = 1012056614238441605
+FESTIVAL_CHANNEL_ID = 1054704196206727261
 
 # What happens if the bot shuts down??
 # Multiple voice channels?? Idea: self.performer as a dictionary with channel ID as keys, channel checking as a list
@@ -13,15 +13,17 @@ class VoiceManager(commands.Cog):
 
     def __init__(self,bot) -> None:
         self.bot = bot
-        self.performer = None # Stores the user ID of the performer
+        self.channels = [GENERAL_CHANNEL_ID, FESTIVAL_CHANNEL_ID]
+        self.performer = {GENERAL_CHANNEL_ID:None,
+                        FESTIVAL_CHANNEL_ID:None} # Stores the channel ID:user ID of the performer
 
     @commands.command(name="invoice")
     async def invoice(self, ctx):
-        voiceChannelName = ctx.author.voice.channel
-        if voiceChannelName:
+        if ctx.author.voice:
+            voiceChannelName = ctx.author.voice.channel
             await ctx.send(f"You are in the voice channel: {voiceChannelName}")
         else:
-            ctx.send(f"You are currently not in a voice/stage channel {ctx.author.mention}")
+            await ctx.send(f"You are currently not in a voice/stage channel {ctx.author.mention}")
     
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -34,7 +36,8 @@ class VoiceManager(commands.Cog):
         # 3. You are leaving the vc to outside
         # 4. You are leaving the vc to some other vc
 
-        if after.channel is None and before.channel.id == GENERAL_CHANNEL_ID:
+        if after.channel is None and before.channel.id in self.channels:
+            channelID = before.channel.id
             # Case 3:
             # Leaving the vc -> remove audience and performer
             # if they are a performer -> also reset performer
@@ -57,10 +60,11 @@ class VoiceManager(commands.Cog):
                 print("THE PERFORMER HAS LEFT.")
                 myRoleobj = discord.Object(id=PERFORMER_ROLE_ID)
                 await member.remove_roles(myRoleobj)
-                self.performer = None
+                self.performer[channelID] = None
                 print("REMOVED PERFORMANCE ROLE")
 
-        elif after.channel.id == GENERAL_CHANNEL_ID:
+        elif after.channel.id in self.channels:
+            channelID = after.channel.id
             # Case 1 and 2:
             # Entering the vc -> make audience
 
@@ -81,7 +85,8 @@ class VoiceManager(commands.Cog):
             if isPerformer:
                 pass
         
-        elif before.channel.id == GENERAL_CHANNEL_ID:
+        elif before.channel.id in self.channels:
+            channelID = before.channel.id
             # Case 4:
             # Leaving the vc -> remove audience and performer
             # if they are a performer -> also reset performer
@@ -104,18 +109,19 @@ class VoiceManager(commands.Cog):
                 print("THE PERFORMER HAS LEFT.")
                 myRoleobj = discord.Object(id=PERFORMER_ROLE_ID)
                 await member.remove_roles(myRoleobj)
-                self.performer = None
+                self.performer[channelID] = None
                 print("REMOVED PERFORMANCE ROLE")
 
     @commands.command(name="getperformer")
     async def getperformer(self,ctx):
         # Checking if they are in the vc
         if ctx.author.voice:
-            if ctx.author.voice.channel.id == GENERAL_CHANNEL_ID:
-                if self.performer is None:
+            if ctx.author.voice.channel.id in self.channels:
+                channelID = ctx.author.voice.channel.id
+                if self.performer[channelID] is None:
                     # Adding the performer role
                     myRoleobj = discord.Object(id=PERFORMER_ROLE_ID)
-                    self.performer = ctx.author.id
+                    self.performer[channelID] = ctx.author.id
                     await ctx.author.add_roles(myRoleobj)
                     print("ADDED PERFORMER ROLE.")
                     # Removing the pre-added audience role
@@ -125,15 +131,15 @@ class VoiceManager(commands.Cog):
                 else:
                     await ctx.send(f"Sorry, the role is taken for this channel by <@{self.performer}>. To reset the role, please ask them to leave the channel.")
         else:
-            # This doesn't work.
             await ctx.send("You are not in the voice channel.")
 
     @commands.command(name="resetperformer")
     async def resetperformer(self,ctx):
         # Checking if they are in the vc
         if ctx.author.voice:
-            if ctx.author.voice.channel.id == GENERAL_CHANNEL_ID:
-                if self.performer is None:
+            if ctx.author.voice.channel.id in self.channels:
+                channelID = ctx.author.voice.channel.id
+                if self.performer[channelID] is None:
                     await ctx.send("There is no performer to reset.")
                 else:
                     # Removing the performer role
@@ -144,7 +150,7 @@ class VoiceManager(commands.Cog):
                     myRoleobj = discord.Object(id=AUDIENCE_ROLE_ID)
                     await ctx.author.add_roles(myRoleobj)
                     print("ADDED AUDIENCE ROLE")
-                    self.performer = None
+                    self.performer[channelID] = None
         else:
             await ctx.send("You are not in the voice channel.")
 
